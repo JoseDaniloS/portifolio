9a15b77a0144dac63cebed2e4d0136c5bf24c051
Jose Danilo <danilojose.1d@gmail.com>
2024-11-05 10:14:31 -0300

    feat: animation hover projects

--- src/components/projects/project_components.jsx
+++ src/components/projects/project_components.jsx
@@ -10,7 +10,7 @@ function PComponents({ image, nome, link_projeto, link_github, tipo }) {
             backgroundColor: "rgba(0, 0, 0, 0.5)",
             visibility: !hover ? "hidden" : "visible"
         }}
-        className="flex flex-col w-full justify-center items-start p-5 font-bold gap-3 rounded-[5px]"
+        className="flex flex-col w-full justify-center animate-BaixoParaCima items-start p-5 font-bold gap-3 rounded-[5px]"
       >
         <p className="text-[20px] font-bold text-white">{nome}</p>
         <div className=" flex gap-5 text-[13px]">
@@ -36,7 +36,7 @@ function PComponents({ image, nome, link_projeto, link_github, tipo }) {
             backgroundColor: "rgba(0, 0, 0, 0.5)",
             visibility: hover ? "visible" : "hidden",
         }}
-        className="flex flex-col w-full justify-center items-start p-5 font-bold gap-3 rounded-[5px]"
+        className="flex flex-col animate-BaixoParaCima w-full justify-center items-start p-5 font-bold gap-3 rounded-[5px]"
       >
         <p className="text-[20px] font-bold text-white">{nome}</p>
         <div className=" flex gap-5 text-[13px]">
@@ -58,7 +58,7 @@ function PComponents({ image, nome, link_projeto, link_github, tipo }) {
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
-      className="h-[250px] w-[400px] max-md:w-full  flex flex-col items-center gap-1 rounded-[10px] justify-end"
+      className="h-[250px] w-[400px] hover:scale-110 transition-transform max-md:w-full overflow-hidden  flex flex-col items-center gap-1 rounded-[10px] justify-end"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
